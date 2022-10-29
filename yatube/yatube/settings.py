480a3360c2cec0d4eab09dd09c124c4cb63b9679
Nikita <nikitaloskutov18@gmail.com>
2022-10-29 12:40:06 +0300

    Добавлены новые функции коментариев и подписок

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&fbe%ls14^ot=d%jf%@5c#q&rja7_lp+_djma^d_pl+oh9*4(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
